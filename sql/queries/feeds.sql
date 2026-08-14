41d53b038d870e231a547b456f86a04e6293ea1c
davidheeren <davidmheeren@gmail.com>
2026-08-13 21:28:58 -0500

    add lastfetched field in feeds

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -17,3 +17,9 @@ INNER JOIN users ON feeds.user_id = users.id;
 
 -- name: GetFeed :one
 SELECT * FROM feeds WHERE url = $1;
+
+-- name: MarkFeedFetched :one
+UPDATE feeds
+SET last_fetched_at = $2, updated_at = $2
+WHERE id = $1
+RETRUNING *;
